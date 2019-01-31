252bb7cba866effdd8453f850973e345346deb80
Matt Migliore <mmigliore311@gmail.com>
2019-01-31 13:58:58 -0500

    add fetch for user on profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Profile extends Component {
+  state = {
+    user: {}
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:4000/api/v1/users/${this.props.currentUserId}`)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        user: data
+      })
+    })
+  }
 
   render() {
     return (
       <div>
-        <h2>Profile Page</h2>
+        <h2>Profile Page of User#: {this.state.user.id}</h2>
       </div>
     );
   }
 
 }
 
-export default Profile;
+const mapStateToProps = state => {
+  return {
+    currentUserId: state.currentUserId
+  }
+}
+
+export default connect(mapStateToProps)(Profile)
