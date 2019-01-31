826cdaa0cc7e4a55b55d0d907da04b2a0a8d79b1
Matt Migliore <mmigliore311@gmail.com>
2019-01-31 13:22:56 -0500

    add inti profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import { connect } from 'react-redux'
 import Home from './containers/Home'
+import Profile from './containers/Profile'
 import IngredientContainer from './containers/IngredientContainer'
 import RecipeContainer from './containers/RecipeContainer'
 
@@ -16,6 +17,7 @@ class App extends Component {
           <Route path="/" exact component={Home} />
           <Route path="/ingredients" component={IngredientContainer} />
           <Route path="/recipes" component={RecipeContainer} />
+          <Route path="/profile" component={Profile} />
         </div>
 
       </Router>
