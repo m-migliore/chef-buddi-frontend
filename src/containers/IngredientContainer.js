6878f138a4211affa65cb7b2666e245d6fe998cc
Matt Migliore <mmigliore311@gmail.com>
2019-01-31 12:44:51 -0500

    add ingredient filter

--- src/containers/IngredientContainer.js
+++ src/containers/IngredientContainer.js
@@ -4,22 +4,50 @@ import Ingredient from '../components/Ingredient'
 import withSelect from '../hocs/withSelect'
 
 class IngredientContainer extends Component {
+  state = {
+    ingredients: [],
+    filter: ""
+  }
+
   componentDidMount() {
     fetch("http://localhost:4000/api/v1/ingredients")
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      return this.props.loadIngredients(data)
+      //this.props.loadIngredients(data)
+      this.setState({
+        ingredients: data
+      })
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      filter: e.target.value
     })
   }
 
   render() {
     // const SelectableIngredient = withSelect(Ingredient)
+    const filteredIngredients = this.state.ingredients.filter(ingred => ingred.name.includes(this.state.filter))
 
     return (
       <div>
         <h2>IngredientContainer</h2>
-        {this.props.ingredients.map(ingred => {
+        <div className="filter-form">
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.filter}
+            placeholder="Filter by ingredient"
+          />
+        </div>
+        {/* {this.state.ingredients.map(ingred => {
+          // return <SelectableIngredient key={ingred.id} ingred={ingred} selectable={true}/>
+          return <Ingredient key={ingred.id} ingred={ingred}/>
+        })} */}
+
+        {filteredIngredients.map(ingred => {
           // return <SelectableIngredient key={ingred.id} ingred={ingred} selectable={true}/>
           return <Ingredient key={ingred.id} ingred={ingred}/>
         })}
