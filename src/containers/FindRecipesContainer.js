725911f53cc6743d89624d5df328d7cb908c9546
Matt Migliore <mmigliore311@gmail.com>
2019-02-01 14:38:06 -0500

    add ingredient list button prompts

--- src/containers/FindRecipesContainer.js
+++ src/containers/FindRecipesContainer.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class FindRecipesContainer extends Component {
+  handleClick = e => {
+    if (e.target.name === "user") {
+      console.log("user ingredents")
+    } else {
+      console.log("all ingredients")
+    }
+  }
 
   render() {
     return (
-      <div>Find Recipes</div>
+      <div>
+        <h2>Step 1. Select Ingredients</h2>
+        <button name="user" onClick={this.handleClick}>Your Ingredients</button>
+        <button name="all" onClick={this.handleClick}>All Ingredients Ingredients</button>
+      </div>
     );
   }
 
