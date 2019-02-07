4993037f7b371e402ae96f4e5d29826bd2063f90
Matt Migliore <mmigliore311@gmail.com>
2019-02-07 11:51:02 -0500

    add init signup form with switch to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,7 +71,7 @@ class Login extends Component {
               />
             </div>
           </div>
-          <button className="btn btn-primary" type="submit">Submit</button>
+          <button className="btn btn-primary" type="submit">Login</button>
         </form>
       </div>
       <div className="col-12">
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Signup extends Component {
+  state = {
+    username: "",
+    password: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log("signup")
+  }
+
+  handleSwitch = e => {
+    e.preventDefault()
+    this.props.setHomeFormType()
+  }
 
   render() {
     return (
-      <div>
-        signup
+      <>
+      <div className="col-12">
+        <h2>Signup</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="form-group col-md-6">
+              <label htmlFor="username-input">Username</label>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                placeholder="Enter Username"
+                onChange={this.handleChange}
+                id="username-input"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="password-input">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                placeholder="Enter Password"
+                onChange={this.handleChange}
+                id="password-input"
+                className="form-control"
+              />
+            </div>
+          </div>
+          <button className="btn btn-primary" type="submit">Signup</button>
+        </form>
       </div>
+      <div className="col-12">
+        <a href="#" onClick={this.handleSwitch}>Login</a>
+      </div>
+      </>
     );
   }
 
 }
 
-export default Signup;
+const mapDispatchToProps = dispatch => {
+  return{
+    setHomeFormType: () => dispatch({type: "SET_HOME_FORM_TYPE", payload: "login"})
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Signup)
